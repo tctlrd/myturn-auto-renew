7a6de83a8bbc696e1196ec73a2ee12ca0d27558b
tctlrd <tctlrd@noreply.codeberg.org>
2024-08-31 16:45:22 +0000

    ssl

--- renew.sh
+++ renew.sh
@@ -3,8 +3,8 @@
 # this script will renew all myturn.com checked out items to the max
 
 # it requires that you have curl and jq installed on your machine
-# set the login information variables below or the script will promt you for them
-# you can optionally have the script retain your answers to the promts
+# set the login information variables below or the script will prompt you for them
+# you can optionally have the script retain answers to the prompts which is slightly more secure
 
 # syntax: ./renew.sh [-l|p|c|d|i]
 # options:
@@ -25,7 +25,7 @@ CURRENT=current-loans.json
 
 # if the username and password variables are set in this file, remove them from the env file if it exists
 [ -n "$USERNAME" ] && [ -f env-renew.sh ] && sed -i '/USERNAME/d' env-renew.sh
-[ -n "$PASSWORD" ] && [ -f env-renew.sh ] && sed -i '/PASSWORD/d' env-renew.sh
+[ -n "$PASSWORD" ] && [ -f env-renew.sh ] && sed -i '/WORD/d' env-renew.sh
  
 # if the domain variable is set in this file remove it from the env file
 [ -n "${DOMAIN}" ] && [ -f env-renew.sh ] && sed -i '/DOMAIN/d' env-renew.sh
@@ -41,13 +41,15 @@ else BASE=""; fi
 # function to ask the user to set the domain variable and optionally remember it
 setDomain(){
   if [ -z "$DOMAIN" ]; then
-    echo "kindly enter the domain of your myTurn site (ie. xxx.myturn.com)"
+    echo "kindly enter the domain of your myTurn site"
+    echo "(ie. xyz.myturn.com)"
     read -p "domain: " DOMAIN
     BASE="https://${DOMAIN}/library/"
-    echo "enter 'y' to store this variable; leave blank to skip; run the script with -d to clear"
-    read -p "remember: " MEM_D
+    read -p "Do you want to save this domain? (y/n): " MEM_D
     [ $MEM_D == "y" ] && echo "export DOMAIN=\"$DOMAIN\"" >> env-renew.sh
   fi
+  DOMAIN_T=$(printf "%s" "$DOMAIN" | openssl dgst -sha256 | cut -d ' ' -f 2)
+  [ -n "$PASSWORD" ] && PASSWORD=$(echo -n "$PASSWORD" | openssl enc -aes-256-cbc -salt -pass pass:"$DOMAIN_T" -pbkdf2 -base64)
 }
 
 # function to login to the server and save the session in a cookie file
@@ -58,19 +60,16 @@ Login() {
     MEM_U=true
     [ -f env-renew.sh ] && sed -i -e '/USERNAME/d' -e '/PASSWORD/d' env-renew.sh
   fi
+  [ -n "$WORD" ] && PASSWORD="$WORD"
   if [ -z "$PASSWORD" ]; then
-  read -s -p "password: " PASSWORD && echo
-  echo "enter 'y' to store login details; leave blank to skip; run the script with -d to clear"
-  read -p "remember: " MEM_L
-  fi
-  if [[ $MEM_L == "y" ]]; then
-    [ -f env-renew.sh ] && sed -i -e '/USERNAME/d' -e '/PASSWORD/d' env-renew.sh
-    [[ $MEM_U == true ]] && echo "export USERNAME=\"$USERNAME\"" >> env-renew.sh
-    echo "export PASSWORD=\"$PASSWORD\"" >> env-renew.sh
+  read -s -p "password: " PASSWORD
+  PASSWORD=$(echo -n "$PASSWORD" | openssl enc -aes-256-cbc -salt -pass pass:"$DOMAIN_T" -pbkdf2 -base64)
+  echo
+  read -p "Do you want to save these credentials? (y/n): " MEM_L
   fi
   echo "attempting to log into $DOMAIN as user \"${USERNAME}\""
   [ -f cookie ] && mv cookie cookie_bkp
-  SESH=$(curl ${BASE}j_spring_security_check -d "j_username=${USERNAME}&j_password=${PASSWORD}" -c cookie -s -w "%header{location}")
+  SESH=$(curl ${BASE}j_spring_security_check -d "j_username=${USERNAME}&j_password=$(echo "$PASSWORD" | openssl enc -d -aes-256-cbc -salt -pass pass:"$DOMAIN_T" -pbkdf2 -base64)" -c cookie -s -w "%header{location}")
   if [[ "$SESH" == *"authfail"* ]]; then
     [ -f env-renew.sh ] && sed -i -e '/USERNAME/d' -e '/PASSWORD/d' env-renew.sh
     echo "login has failed; check your username and password"
@@ -78,6 +77,11 @@ Login() {
     exit 1
   elif [[ "$SESH" == *"library"* ]]; then
     echo "login success!"
+    if [[ $MEM_L == "y" ]]; then
+      [ -f env-renew.sh ] && sed -i -e '/USERNAME/d' -e '/PASSWORD/d' env-renew.sh
+      [[ $MEM_U == true ]] && echo "export USERNAME=\"$USERNAME\"" >> env-renew.sh
+      echo "export WORD=\""$PASSWORD"\"" >> env-renew.sh
+    fi
     [ -f cookie_bkp ] && rm -f cookie_bkp
     [ -f env-renew.sh ] && source env-renew.sh
     [ -z "$MEMBERSHIP_ID" ] && getId
@@ -185,6 +189,5 @@ while getopts ":lpcdh" opt; do
   \?) echo "Invalid option: -${OPTARG}."; exit 1;;
   esac
 done
-
 # if no options are called default to running the renew function
 Renew
